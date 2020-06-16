e32574d09e37dfbc24322d99775e880e82363eaa
Funkschy <felix.schoeller@protonmail.com>
2020-06-16 23:32:51 +0200

    improvements

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,59 @@
 import * as vscode from 'vscode';
-import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
+import { ProviderResult } from 'vscode';
+import * as proc from 'child_process';
 
-import {LLDBDebugSession} from './lldb';
+import { LLDBDebugSession } from './lldb';
 
 export function activate(context: vscode.ExtensionContext) {
 	const provider = new ConfigurationProvider();
-	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('lldb-mi', provider));
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('kantan', provider));
 
 	const factory = new InlineDebugAdapterFactory();
-	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('lldb-mi', factory));
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('kantan', factory));
 	if ('dispose' in factory) {
 		context.subscriptions.push(factory);
 	}
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
 
 class ConfigurationProvider implements vscode.DebugConfigurationProvider {
 	resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
 		if (!config.type && !config.request && !config.name) {
 			const editor = vscode.window.activeTextEditor;
 			if (editor && editor.document.languageId === 'kantan') {
-				config.type = 'lldb-mi';
+				config.type = 'kantan';
 				config.name = 'Launch';
 				config.request = 'launch';
-				config.program = '${config.cwd}/a.out';
+				config.program = `${folder?.uri.path}/a.out`;
 				config.args = [];
-				config.stopOnEntry = true;
 			}
 		}
 
-		if (!config.program) {
+		if (!config.compile && !config.program) {
 			return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
 				return undefined;	// abort launch
 			});
+		}
+
+		if (config.compile === true) {
+			let args = ['-g'].concat(config.kantanFiles);
+			let compilerOut = proc.spawnSync(config.kantanPath, args, { cwd: folder?.uri.path, env: process.env });
+
+			if (compilerOut.error) {
+				return vscode.window.showInformationMessage(compilerOut.error.toString()).then(_ => {
+					return undefined;	// abort launch
+				});
+			}
+
+			if (compilerOut.status !== 0) {
+				return vscode.window.showInformationMessage(compilerOut.output.toString()).then(_ => {
+					return undefined;	// abort launch
+				});
+			}
+
+			config.program = `${folder?.uri.path}/a.out`;
 		}
 
 		return config;
